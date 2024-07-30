06493aa76c8eb2ce57f051f92b7ff22f537d89f4
coder om <mail.coderom@gmail.com>
2024-07-30 13:46:41 +0530

    Update backend base url

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const devBaseURL='http://localhost:5000/api/v1';
-const proBaseURL='https://yt-studio-manager.onrender.com/api/v1';
+const proBaseURL='https://yt-vdo-manager.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
     baseURL: proBaseURL,
