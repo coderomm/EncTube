b7fe285369b5bebe367148b47b92395d30af7e6b
coder om <mail.coderom@gmail.com>
2024-08-19 22:42:01 +0530

    allowedOrigins

--- server/app.js
+++ server/app.js
@@ -21,8 +21,13 @@ dbConnect();
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  `${process.env.FRONTEND_URL}`,
+  'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: allowedOrigins,
   credentials: true
 }));
 
