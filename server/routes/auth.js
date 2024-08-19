ae1df13a6750afd6b4a22ac84917ede58efe29b6
coder om <mail.coderom@gmail.com>
2024-08-19 21:52:56 +0530

    debug

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -31,7 +31,7 @@ router.get('/oauth2callback', async (req, res) => {
   console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET);
   console.log('REDIRECT_URL:', process.env.REDIRECT_URL);
 
-  console.log('OAUTH2_CLIENT_ID:', CLIENT_ID);
+  console.log('OAUTH2_CLIENT_ID:', OAUTH2_CLIENT_ID);
   console.log('OAUTH2_CLIENT_SECRET:', OAUTH2_CLIENT_SECRET);
   console.log('OAUTH2_REDIRECT_URL:', OAUTH2_REDIRECT_URL);
 
